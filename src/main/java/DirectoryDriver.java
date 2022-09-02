33f625ba7a7651ed6be2aedc7cb7bc8ae886c608
Katie Cleland <kcleland0818@gmail.com>
2022-09-02 12:03:11 -0600

    Modify lab

---
+++ src/main/java/DirectoryDriver.java
@@ -0,0 +1,13 @@
+ class DirectoryDriver {
+    public static void main(String[] args) {
+        // Your code here
+    }
+
+    public static void printMenu() {
+        System.out.println("Welcome to the Global Directory! What would you like to do?");
+        System.out.println("0. Exit");
+        System.out.println("1. Add a City to the Directory");
+        System.out.println("2. Show the Cities in the Directory");
+        System.out.println();
+    }
+}
